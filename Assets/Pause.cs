21ec3e009a7e25056e0049f38a6a7ffc3ecf2ca3
LinearHaru <S2Haru@outlook.outlook.com>
2021-07-30 00:24:32 -0300

    menu de pausa começado

---
+++ Assets/Pause.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject menuHolder;
+    void Start()
+    {
+        Time.timeScale = 1;
+        menuHolder.active = false;
+    }
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause")) {
+            if (menuHolder.active){
+                Time.timeScale = 1;
+                menuHolder.active = false;
+            } else {
+                Time.timeScale = 0;
+                menuHolder.active = true;
+            }
+        }
+    }
+}
---
+++ Assets/TutorialAlert.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialAlert : MonoBehaviour
+{
+    Animator anim;
+    public bool disableOnExit = true;
+    public bool destroyOnExit = true;
+
+    public string tutorial = "";
+    void Start()
+    {
+        anim = GameObject.FindGameObjectWithTag("Tutorial").GetComponent<Animator>();   
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.tag == "Player")
+        {
+            anim.Play(tutorial,1);
+            anim.SetBool("open", true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (disableOnExit)
+                anim.SetBool("open", false);
+            if (destroyOnExit)
+                Destroy(this.gameObject);
+        }
+    }
+    private void OnDestroy()
+    {
+        anim.SetBool("open", false);
+    }
+}
